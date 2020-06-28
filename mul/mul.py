3cd61c8f7e871ca7c8f030aa21c231231ae0a894
major major <majorx234@gmail.com>
2020-06-28 02:29:52 +0200

    removed printf

--- mul/mul.py
+++ mul/mul.py
@@ -1,12 +1,11 @@
 import sys
 
 def mul(x,y):
-    print("mul: %d" % x*y)
     return x*y
 
 def main():    
     if len(sys.argv) == 3:
-        mul(int(sys.argv[1]),int(sys.argv[2]))
+        printf(mul(int(sys.argv[1]),int(sys.argv[2])))
     else:
         print("ERROR:",str(len(sys.argv)-1)," arguments given instead of 2")
 
